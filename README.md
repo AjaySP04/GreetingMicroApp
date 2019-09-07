133eeef12ee156d3f59c9fc5c2d63179cf490802
Ajay Singh Parmar <ajays.parmar04@gmail.com>
2019-09-08 02:35:35 +0530

    Changed logic to make compatible with latest redis allowed inputs.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Greeting to Microservices Using Python
+
+- We have used Nameko Rpc based python frameword to build services for demostrating microservices working.
+- We have use AMQP as RabbitMQ.
+- Data storage we have use in memory based Redis data storage.
--- TripService/trip_service.py
+++ TripService/trip_service.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import uuid
+import json
 
 from nameko.rpc import rpc
 from nameko_redis import Redis
@@ -19,10 +20,19 @@ class TripService:
 
 	@rpc
 	def create(self, airport_from_id, airport_to_id):
-		""""""
-		trip_id = uuid.uuid4.hex
-		self.redis.set(trip_id, {
+		trip_id = uuid.uuid4().hex
+		self.redis.set(trip_id, json.dumps({
 			"from": airport_from_id,
 			"to": airport_to_id
-			})
+			}))
 		return trip_id
+
+if __name__=='__main__':
+
+	from_id = "559e8fdf4d26426f8b799b53dc5b34a6"
+	to_id = "30181283b44b400a8a0ab069f3049af4"
+
+	trip_1 = TripService()
+	trip_1.create(from_id, to_id)
+
+	pass
